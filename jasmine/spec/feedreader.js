f0c792ee995d48035c71d3d09b979d678caf80b4
Daniel <daniel.login@ymail.com>
2016-11-03 21:37:13 +0100

    11

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -60,16 +60,21 @@ $(function() {
 
     describe('The Menu', function() {
 
-        
+         /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the
+         * hiding/showing of the menu element. (11)
+         */
+
+         it('menu element is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
+
 
     });
 
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
